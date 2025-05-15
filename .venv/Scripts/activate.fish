5c148908c7d71ed9c66b7cb85280cf9e436c86e6
Jonathas <jonathas10pereira@gmail.com>
2025-05-15 20:08:36 -0300

    15/05 2

--- .venv/Scripts/activate.fish
+++ .venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\User\Desktop\Projetos\biblioteca\.venv'
+set -gx VIRTUAL_ENV 'C:\Users\UNIVASSOURAS\Desktop\biblioteca\sharebook\.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
